a9bbd1b7ccc47786f520ccb43c656d820a4049d5
SVega9848 <santivediap@gmail.com>
2023-05-11 17:27:46 +0200

    Add localStorage functions

--- script.js
+++ script.js
@@ -2,4 +2,32 @@
 
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
+
+    // Add localStorage functions
+
+    const userName = event.target.name.value;
+    const userEmail = event.target.email.value;
+    const userMessage = event.target.message.value;
+    const userURL = event.target.url.value;
+
+    let usersList = [];
+
+    const userProfile = {name: userName, email: userEmail, message: userMessage, url: userURL}
+
+    if(localStorage.users) {
+
+        // Existing users condition
+
+        let users = JSON.parse(localStorage.users)
+        usersList = [];
+        usersList.push(...users)
+        usersList.push(userProfile)
+        localStorage.users = JSON.stringify(usersList)
+    } else {
+
+        // Non existing users condition
+
+        usersList.push(userProfile);
+        localStorage.users = JSON.stringify(usersList);
+    }
 })
